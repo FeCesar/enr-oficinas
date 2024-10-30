6690beee9b9407d2fadba9ba207e591e3cb052e4
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2024-10-29 21:51:50 -0300

    Projeto Base

---
+++ src/main/java/com/enr/project/EnrApplication.java
@@ -0,0 +1,13 @@
+package com.enr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrApplication.class, args);
+	}
+
+}
